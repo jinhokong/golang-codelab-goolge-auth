55a39acd0920ae171acb6649c9014781a041105f
jinho hong <tpdleps@gmail.com>
2019-03-30 17:46:12 +0900

    요청한 결과를 Session에 저장하여 관리

--- handler/auth.go
+++ handler/auth.go
@@ -1,10 +1,67 @@
 package handler
 
 import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/golangkorea/codelab/model"
+	"github.com/golangkorea/codelab/oauth"
+	"github.com/gorilla/sessions"
 )
 
 func GoogleOAuthCallBack(w http.ResponseWriter, r *http.Request) {
+	session, err := store.Get(r, "auth")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	state := session.Values["state"]
+	delete(session.Values, "state")
+
+	if state != r.FormValue("state") {
+		http.Error(w, "invalid session state", http.StatusUnauthorized)
+		return
+	}
+
+	token, err := oauth.GoogleOAuthConf.Exchange(context.Background(), r.FormValue("code"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	client := oauth.GoogleOAuthConf.Client(context.Background(), token)
+	userInfoResp, err := client.Get(oauth.GoogleUserInfoAPI)
+
+	defer userInfoResp.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	if userInfoResp.StatusCode != http.StatusOK {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	userInfo, err := ioutil.ReadAll(userInfoResp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var user model.User
+	json.Unmarshal(userInfo, &user)
+
+	session.Options = &sessions.Options{
+		Path:   "/",
+		MaxAge: 86400,
+	}
+	session.Values["user"] = user
+
+	session.Save(r, w)
+
 	http.Redirect(w, r, "/profile", http.StatusFound)
 	return
 }
--- handler/render.go
+++ handler/render.go
@@ -1,16 +1,23 @@
 package handler
 
 import (
+	"encoding/gob"
 	"html/template"
 	"net/http"
 
+	"github.com/golangkorea/codelab/model"
 	"github.com/golangkorea/codelab/oauth"
+	"github.com/gorilla/sessions"
 )
 
 var tmpl *template.Template
+var store *sessions.CookieStore
 
 func init() {
+	store = sessions.NewCookieStore([]byte("very-secret-key"))
 	tmpl = template.Must(template.ParseGlob("templates/*.html"))
+
+	gob.Register(model.User{})
 }
 
 func RenderIndex(w http.ResponseWriter, r *http.Request) {
@@ -19,10 +26,33 @@ func RenderIndex(w http.ResponseWriter, r *http.Request) {
 
 func RenderLogin(w http.ResponseWriter, r *http.Request) {
 	state := "randomTest"
+
+	session, _ := store.Get(r, "auth")
+	session.Options = &sessions.Options{
+		Path:   "/auth",
+		MaxAge: 300,
+	}
+	session.Values["state"] = state
+	session.Save(r, w)
+
 	url := oauth.GoogleAuthorizationURL(state)
 	tmpl.ExecuteTemplate(w, "login.html", url)
 }
 
 func RenderProfile(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "profile.html", nil)
+	session, err := store.Get(r, "auth")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if _, ok := session.Values["user"]; !ok {
+		http.Error(w, "page not found", http.StatusNotFound)
+		return
+	}
+
+	user := session.Values["user"].(model.User)
+
+	tmpl.ExecuteTemplate(w, "profile.html", user)
+
 }
