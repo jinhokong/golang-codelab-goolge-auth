f1c7058c1791a752cba5a4f0e33b521182f5c1df
jinho hong <tpdleps@gmail.com>
2019-03-30 16:06:24 +0900

    template Parse 를 Group으로 처리하도록합니다.

--- handler/render.go
+++ handler/render.go
@@ -5,29 +5,23 @@ import (
 	"net/http"
 )
 
+var tmpl *template.Template
+
+func init() {
+	tmpl = template.Must(template.ParseGlob("templates/*.html"))
+}
+
 // RenderIndex Just Rendering HTML template
 func RenderIndex(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		panic(err)
-	}
-	tmpl.Execute(w, nil)
+	tmpl.ExecuteTemplate(w, "index.html", nil)
 }
 
 // RenderLogin Just Rendering HTML template
 func RenderLogin(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/login.html")
-	if err != nil {
-		panic(err)
-	}
-	tmpl.Execute(w, nil)
+	tmpl.ExecuteTemplate(w, "login.html", nil)
 }
 
 // RenderProfile Just Rendering HTML template
 func RenderProfile(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/profile.html")
-	if err != nil {
-		panic(err)
-	}
-	tmpl.Execute(w, nil)
+	tmpl.ExecuteTemplate(w, "profile.html", nil)
 }
