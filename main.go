5022d6c250c7f61cc555a81648002ed842033f00
jinho hong <tpdleps@gmail.com>
2019-03-30 15:43:04 +0900

    RenderIndex function 분리

--- main.go
+++ main.go
@@ -10,13 +10,16 @@ const bind = ":1333"
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
+	http.HandleFunc("/", RenderIndex)
 	fmt.Printf("Server is listening on %s\n", bind)
 
 	http.ListenAndServe(bind, nil)
 
 	log.Fatal(http.ListenAndServe(bind, nil))
+}
 
+// RenderIndex Just Rendering HTML template
+func RenderIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+	return
 }
