ed5d4abc906b9debb625e0208da6a44a181e262e
jinho hong <tpdleps@gmail.com>
2019-03-30 16:38:42 +0900

    주석 삭제

--- handler/auth.go
+++ handler/auth.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 )
 
-// GoogleOAuthCallBack a
 func GoogleOAuthCallBack(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/profile", http.StatusFound)
 	return
--- handler/render.go
+++ handler/render.go
@@ -13,19 +13,16 @@ func init() {
 	tmpl = template.Must(template.ParseGlob("templates/*.html"))
 }
 
-// RenderIndex Just Rendering HTML template
 func RenderIndex(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "index.html", nil)
 }
 
-// RenderLogin Just Rendering HTML template
 func RenderLogin(w http.ResponseWriter, r *http.Request) {
 	state := "randomTest"
 	url := oauth.GoogleAuthorizationURL(state)
 	tmpl.ExecuteTemplate(w, "login.html", url)
 }
 
-// RenderProfile Just Rendering HTML template
 func RenderProfile(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "profile.html", nil)
 }
